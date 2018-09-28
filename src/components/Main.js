bdc6d010d6834a61d4e80c75792ea4d9e3fcf609
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-09-28 14:15:57 +0900

    Added LoginPage.js and made Login.js its child component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Login';
 import SignUp from './SignUp';
+import LoginPage from './LoginPage';
 
 
 const Main = () => (
     <main>
         <Switch>
-            <Route exact path='/' component={Login}/>
+            <Route exact path='/' component={LoginPage}/>
             <Route exact path='/SignUp' component={SignUp}/>
         </Switch>
     </main>
