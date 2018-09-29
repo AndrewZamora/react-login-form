e240d5216b2da92095f5c1c5f043bf78354efc34
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-09-30 02:37:26 +0900

    added onSubmit to main component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import Main from './components/Main';
 import './App.css';
 import Navbar from './components/Navbar';
 
+
+
 class App extends Component {
+    handleSubmit = (event) => {
+        // Connect to backend
+        console.log(event.target.value)
+        alert(`E-mail: ${this.state.email}, Password: ${this.state.password}`);
+        event.preventDefault();
+    }
     render() {
         return (
             <div>
                 <Navbar />
                 <div className="container">
-                    <Main />
+                    <Main onSubmit={this.handleSubmit} />
                 </div>
             </div>
         );
