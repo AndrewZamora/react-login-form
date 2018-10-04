0948275eeafff1c805414e615babb4bfa3cb87f4
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-10-04 22:06:55 +0900

    Removed constructor because I don't need function binding

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-    }
 
     handleSubmit = (event, submitInfo) => {
         // Connect to backend
