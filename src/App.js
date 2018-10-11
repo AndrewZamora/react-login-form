9c793e60c7586ce2e8e8ea1ac9ee71ee6b1dec91
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-10-11 23:04:32 +0900

    Changed App.js imports to connect main.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Main from './components/Main';
+import Main from './example/Main';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './example/Navbar';
 
 class App extends Component {
 
